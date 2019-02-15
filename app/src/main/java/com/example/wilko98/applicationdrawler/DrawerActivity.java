3fb47ff4ab04ea0c6b94da9ead4eb810a349d1b5
wilko98 <tamerlankunakbaev@gmail.com>
2019-02-16 00:23:42 +0600

    Initial commit

---
+++ app/src/main/java/com/example/wilko98/applicationdrawler/DrawerActivity.java
@@ -0,0 +1,22 @@
+package com.example.wilko98.applicationdrawler;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class DrawerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+    }
+
+}
